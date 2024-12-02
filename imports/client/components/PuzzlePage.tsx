697dbe22428eaf9ca7af783fd112f8eccb6f414e
Jeff Davidson <jpd@google.com>
2024-12-01 20:19:30 -0800

    Attempt to use linked Google account when loading documents.
    This would hopefully have no effect if the user isn't logged into their
    linked account (it would redirect to the default account, as before),
    but would select the linked account if available, which could prevent
    users from showing up anonymously.

--- imports/client/components/PuzzlePage.tsx
+++ imports/client/components/PuzzlePage.tsx
@@ -915,12 +915,14 @@ const PuzzlePageMetadata = ({
   displayNames,
   document,
   isDesktop,
+  selfUser,
 }: {
   puzzle: PuzzleType;
   bookmarked: boolean;
   displayNames: Map<string, string>;
   document?: DocumentType;
   isDesktop: boolean;
+  selfUser: Meteor.User;
 }) => {
   const huntId = puzzle.hunt;
   const puzzleId = puzzle._id;
@@ -1044,7 +1046,7 @@ const PuzzlePageMetadata = ({
 
   const documentLink =
     document && !isDesktop ? (
-      <DocumentDisplay document={document} displayMode="link" />
+      <DocumentDisplay document={document} displayMode="link" user={selfUser} />
     ) : null;
 
   const editButton = canUpdate ? (
@@ -1749,14 +1751,26 @@ const PuzzleDocumentDiv = styled.div`
 `;
 
 const PuzzlePageMultiplayerDocument = React.memo(
-  ({ document }: { document?: DocumentType }) => {
+  ({
+    document,
+    selfUser,
+  }: {
+    document?: DocumentType;
+    selfUser: Meteor.User;
+  }) => {
     let inner = (
       <DocumentMessage>
         Attempting to load collaborative document...
       </DocumentMessage>
     );
     if (document) {
-      inner = <DocumentDisplay document={document} displayMode="embed" />;
+      inner = (
+        <DocumentDisplay
+          document={document}
+          displayMode="embed"
+          user={selfUser}
+        />
+      );
     }
 
     return <PuzzleDocumentDiv>{inner}</PuzzleDocumentDiv>;
@@ -2012,6 +2026,7 @@ const PuzzlePage = React.memo(() => {
       document={document}
       displayNames={displayNames}
       isDesktop={isDesktop}
+      selfUser={selfUser}
     />
   );
   const chat = (
@@ -2093,7 +2108,10 @@ const PuzzlePage = React.memo(() => {
             {chat}
             <PuzzleContent>
               {metadata}
-              <PuzzlePageMultiplayerDocument document={document} />
+              <PuzzlePageMultiplayerDocument
+                document={document}
+                selfUser={selfUser}
+              />
               {debugPane}
             </PuzzleContent>
           </SplitPanePlus>
